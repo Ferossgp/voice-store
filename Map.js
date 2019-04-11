e2c09dba9ba494b8524fe772dbaf2284ff3110e9
Gheorghe Pinzaru <feross95@gmail.com>
2019-04-11 12:16:52 +0300

    Add more items

--- Map.js
+++ Map.js
@@ -2,30 +2,34 @@ import React, { Component } from 'react';
 import { Image, Dimensions, View, Text, TouchableOpacity } from 'react-native';
 import ImageZoom from 'react-native-image-pan-zoom';
 
+const imageWidth = 815;
+const imageHeight = 2579;
+const {height, width} = Dimensions.get('window');
 export default class App extends Component {
     render() {
         return (
             <View style={{flex: 1}}>
                 <TouchableOpacity 
-                    style={{height: 40, alignItems:"center", justifyContent:"center"}}
+                    style={{height: 40, width: width,  alignItems:"center", justifyContent:"center"}}
                     onPress={this.props.goBack}
                 >
                     <Text style={{fontSize: 18, fontWeight: "bold", textAlign: "center"}}>Go Back</Text>
                 </TouchableOpacity>
+            <View style={{flex: 1}}>
                 <ImageZoom 
-                    cropWidth={Dimensions.get('window').width}
-                    cropHeight={Dimensions.get('window').height}
-                    imageWidth={Dimensions.get('window').width}
-                    imageHeight={Dimensions.get('window').width / 815 *  2579}
-                    centerOn={{...this.state.center, duration: 0, scale: 1}}>
+                    cropWidth={width}
+                    cropHeight={height}
+                    imageWidth={width}
+                    imageHeight={width / imageWidth *  imageHeight}
+                    centerOn={{y: width / imageWidth * this.props.center.y, x: 0, duration: 0, scale: 1}}>
                     <Image 
-                        style={{width:Dimensions.get('window').width, height: Dimensions.get('window').width / 815 *  2579}} 
+                        style={{width: width, height: width / imageWidth *  imageHeight}} 
                         source={require('./map.png')}/>
                 </ImageZoom>
                 <View style={{
                     position: "absolute",
-                    top: this.props.center.y,
-                    left: this.props.center.x,
+                    top: width / imageWidth * this.props.center.y,
+                    left: width / imageWidth * this.props.center.x,
                     width: 32,
                     height: 32,
                     opacity: 0.15,
@@ -35,14 +39,15 @@ export default class App extends Component {
                 </View>
                 <View style={{
                     position: "absolute",
-                    top: this.props.center.y + 11,
-                    left: this.props.center.x + 11,
+                    top: width / imageWidth * this.props.center.y + 11,
+                    left: width / imageWidth * this.props.center.x + 11,
                     height: 10,
                     width: 10,
                     borderRadius: 10,
                     backgroundColor: "#EB5757",
                 }}>
                 </View>
+            </View>
             </View>
         )
     }
--- index.js
+++ index.js
@@ -10,20 +10,46 @@ import MiniSearch from 'minisearch';
 
 // A collection of documents for our examples
 const documents = [
-    { id: 1, title: 'Water'},
-    { id: 2, title: 'Tomato' },
-    { id: 3, title: 'Bread'},
-    { id: 4, title: 'Cheese'},
+    { id: 1, title: 'Cheese', center: {x: 510, y: 40}},
+    { id: 2, title: 'Milk', center: {x: 745, y: 40} },
+    { id: 3, title: 'Bread', center: {x: 235, y: 100}},
+    { id: 4, title: 'Cake', center: {x: 280, y: 40}},
+    { id: 5, title: 'flour', center: {x: 465, y: 230}},
+    { id: 6, title: 'cereal', center: {x: 740, y: 200} },
+    { id: 7, title: 'porridge', center: {x: 740, y: 400}},
+    { id: 8, title: 'kefir', center: {x: 740, y: 300}},
+    { id: 9, title: 'yogurt', center: {x: 740, y: 650}},
+    { id: 10, title: 'ketchup', center: {x: 575, y: 440} },
+    { id: 11, title: 'Eggs', center: {x: 530, y: 700}},
+    { id: 12, title: 'Water Mineral', center: {x: 380, y: 680}},
+    { id: 13, title: 'Pepsi Cola Sprite Fanta Merinda Seven Up 7 Up', center: {x: 340, y: 750}},
+    { id: 14, title: 'Burn, Redbull, Adrenaline Rush', center: {x: 340, y: 880} },
+    { id: 15, title: 'Beer', center: {x: 190, y: 750}},
+    { id: 16, title: 'Chips Nachos Lays pringles', center: {x: 250, y: 750}},
+    { id: 17, title: 'Juice', center: {x: 380, y: 900}},
+    { id: 18, title: 'Cookies', center: {x: 570, y: 900} },
+    { id: 19, title: 'Rice', center: {x: 540, y: 670}},
+    { id: 20, title: 'Pasta Barilla Macarni Spaghetti', center: {x: 720, y: 770}},
+    { id: 21, title: 'mussels octopus ', center: {x: 80, y: 650}},
+    { id: 22, title: 'salted fish', center: {x: 80, y: 725} },
+    { id: 23, title: 'Sausages', center: {x: 300, y: 550}},
+    { id: 24, title: 'Banana', center: {x: 740, y: 2040}},
+    { id: 25, title: 'Apple', center: {x: 743, y: 1760}},
+    { id: 26, title: 'Peanapple', center: {x: 730, y: 1940}},
+    { id: 27, title: 'Tomato', center: {x: 573, y: 1950}},
+    { id: 28, title: 'Coffee', center: {x: 570, y: 1140}},
+    { id: 29, title: 'Tea', center: {x: 721, y: 1140}},
+    { id: 30, title: 'Meat Pork Beef ', center: {x: 375, y: 1480}},
+    { id: 30, title: 'chicken', center: {x: 375, y: 1360}},
+    { id: 31, title: 'chicken', center: {x: 533, y: 1300}},
 ];
 
-const mapPos = {
-    1: {x: 38, y: 1},
-    2: {x: 100, y: 100},
-    3: {x: 80, y: 80},
-    4: {x: 150, y: 80},
-};
+const mapPos = documents.reduce(function(map, obj) {
+    map[obj.id] = obj.center;
+    return map;
+}, {});
 
-let miniSearch = new MiniSearch({ fields: ['title', 'center'] });
+let miniSearch = new MiniSearch({ fields: ['title'] });
  
 miniSearch.addAll(documents);
 
